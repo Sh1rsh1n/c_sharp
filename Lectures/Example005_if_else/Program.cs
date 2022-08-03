2afe4a691fadb679b89db3cb7557ce6d539f9ae6
Sh1rsh1n <a.s.shirshin@mail.ru>
2022-08-04 00:47:57 +0300

    add if else

---
+++ Lectures/Example005_if_else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("input user name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "alex")
+{
+    Console.WriteLine("Hey, this is Alex");
+}
+else
+{
+    Console.Write("Hello, " + username);
+}
