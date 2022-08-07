f50a980eb6f4c2bb9a5c1a0d28bbc8b63f084fd3
Sh1rsh1n <a.s.shirshin@mail.ru>
2022-08-07 10:35:18 +0300

    added task_6, task_7, task_8

---
+++ Seminars/Seminar_1/additional_task/self_study/task_7/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("input first word");
+char firstWord = Convert.ToChar(Console.ReadLine());
+
+switch(firstWord)
+{
+    case 'f': Console.WriteLine("fisika"); break;
+    case 'm': Console.WriteLine("mathimatic"); break;
+    case 'h': Console.WriteLine("history"); break;
+    case 'g': Console.WriteLine("geografy"); break;
+    case 'b': Console.WriteLine("biology"); break;
+    default: Console.WriteLine("incorrect word"); break;
+}
