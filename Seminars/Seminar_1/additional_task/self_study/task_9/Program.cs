ead1d612421b9ddea7af47ac1e121a27bc1399e6
Sh1rsh1n <a.s.shirshin@mail.ru>
2022-08-07 11:26:49 +0300

    added task_9, task_10

---
+++ Seminars/Seminar_1/additional_task/self_study/task_9/Program.cs
@@ -0,0 +1,38 @@
+﻿int num;
+string value;
+bool result;
+
+
+do{
+    Console.Write("Введите номер дня недели от 1 до 7: ");
+    value = Console.ReadLine();
+    result = Int32.TryParse(value, out num);
+}
+while (!(result));
+
+
+if (num >= 1 && num <= 5)
+{
+    switch (num)
+    {
+        case 1: Console.Write("Monday"); break;
+        case 2: Console.Write("Tuesday"); break;
+        case 3: Console.Write("Wednesday"); break;
+        case 4: Console.Write("Thursday"); break;
+        case 5: Console.Write("Friday"); break;
+    }
+    Console.WriteLine(" WorkDay");
+}
+else if (num >= 6 && num <= 7)
+{
+    switch (num)
+    {
+        case 6: Console.Write("Saturday"); break;
+        case 7: Console.Write("Sunday"); break;
+    }
+    Console.WriteLine(" WeekEnd");
+}
+else
+{
+    Console.WriteLine("Incorrect numbers. Try again.");
+}
