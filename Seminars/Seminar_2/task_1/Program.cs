ef26dfca0ce862bdca474c69b4ce8ed95c0111b4
Sh1rsh1n <a.s.shirshin@mail.ru>
2022-08-09 21:24:00 +0300

    Seminar_2 independent work

---
+++ Seminars/Seminar_2/task_1/Program.cs
@@ -0,0 +1,94 @@
+﻿// int number = new Random().Next(10, 100);
+
+// Console.WriteLine("number = " + number);
+// int num1 = number / 10;
+// Console.WriteLine("num1" + num1);
+// int num2 = number % 10;
+// Console.WriteLine("num2" + num2);
+
+// if (num1 > num2)
+// {
+//     Console.WriteLine(num1);
+// }
+// else
+// {
+//     Console.WriteLine(num2);
+// }
+
+
+// int number = new Random().Next(10, 100);
+
+// Console.WriteLine("number = " + number);
+
+// if (nnumber / 10 > number % 10)
+// {
+//     Console.WriteLine(num1);
+// }
+// else
+// {
+//     Console.WriteLine(num2);
+// }
+
+// Задача 11
+// int number = new Random().Next(100, 1000);
+// Console.WriteLine(number);
+
+// int num1 = number / 100;
+// Console.WriteLine(num1);
+
+// int num3 = number % 10;
+// Console.WriteLine(num3);
+
+// Console.WriteLine(num1 * 10 + num3);
+
+
+//Задача 12
+// int n1 = new Random().Next(1, 10);
+// Console.WriteLine(n1);
+// int n2 = new Random().Next(1, 100);
+// Console.WriteLine(n2);
+
+// if (n2 % n1 != 0)
+// {
+//     int oct = n2 % n1;
+//     Console.WriteLine("Не кратно = " + oct);
+// } 
+// else
+// {
+//     Console.WriteLine("Число Кратно");
+
+// }
+
+
+// Задача 14
+// int n1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(n1);
+
+// if((n1 % 7 == 0) && (n1 % 23 == 0))
+// {
+//     Console.WriteLine("Число Кратно");
+// }
+// else
+// {
+//     Console.WriteLine("Число не кратно");
+// }
+
+//Задача 16
+// int n1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(n1);
+// int n2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(n2);
+
+
+// if (n2 == n1 * n1)
+// {
+//     Console.WriteLine("Число " + n2 + " является квадратом " + n1);
+// }
+// else if(n1 == n2 * n2)
+// {
+//     Console.WriteLine("Число " + n1 + " является квадратом " + n2);
+// }
+// else
+// {
+//     Console.WriteLine("Числа " + n1 + "не является квадратом " + n2);
+// }
