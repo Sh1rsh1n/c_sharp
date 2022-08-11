38c23421c6865fd53bfb08efbce2187305ad4a94
Sh1rsh1n <a.s.shirshin@mail.ru>
2022-08-11 22:29:31 +0300

    c# seminar_2 additional task

--- Seminars/Seminar_2/Additional_task/task_4/Program.cs
+++ Seminars/Seminar_2/Additional_task/task_4/Program.cs
@@ -3,3 +3,18 @@
 до тех пор, пока не введена буква z или Z .
 */
 
+const double pi = 3.14;
+
+bool value = true;
+double radius = 0;
+
+while (value)
+{
+    Console.WriteLine("Введите число: ");
+    
+    string str = Console.ReadLine();
+    value = double.TryParse(str, out radius);
+    double squar = pi * (radius * radius);
+
+    Console.WriteLine($"squar = {squar}");
+}
