a6b426c6e36fe0f1a10d97c1a9e0cf140e237cec
Sh1rsh1n <95686701+Sh1rsh1n@users.noreply.github.com>
2022-08-12 14:54:00 +0300

    changes task-6

--- Seminars/Seminar_2/Additional_task/task_6/Program.cs
+++ Seminars/Seminar_2/Additional_task/task_6/Program.cs
@@ -13,4 +13,36 @@ for (int i = 0; i < 10; i++)
         string s2 = array[i] % 2 == 0 ? "четное" : "нечетное";
         Console.WriteLine($"{array[i]} {s2}");
     }
+    /*
+    второй вариант решения: 
+    ***********************
+    
+    for (int i = 0; i < 10; i++)
+	{
+    	int rnd= new Random().Next(-20, 20);
+    	if (rnd >= 0)
+    	{
+        string s2 = rnd % 2 == 0 ? "even" : "odd";
+        Console.WriteLine($"{rnd} {s2}");
+    } 
+    */ 
+    
+    /*
+    третий вариант решения:
+    *******************
+    
+    int count = 0;
+	
+	while(count < 10)
+	{
+		int value = new Random().Next(-20,20);
+		if(value > 0)
+		{
+			string str = value % 2 == 0 ? "even" : "odd";
+			Console.WriteLine($"{value} is {str}");
+		}
+	}
+   
+    */
+    
 }
