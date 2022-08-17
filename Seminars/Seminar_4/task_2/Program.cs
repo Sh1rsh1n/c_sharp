fe6a2789a7294dfadcae28f0b2d562b3cf3ecb74
Sh1rsh1n <a.s.shirshin@mail.ru>
2022-08-17 21:27:12 +0300

    Seminar_4 all tasks

---
+++ Seminars/Seminar_4/task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 28: Напишите программу, которая 
+принимает на вход число N и выдаёт 
+произведение чисел от 1 до N.
+*/
+
+
+int sum(int num)
+{
+    int result = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+
+Console.WriteLine("Input number: ");
+int value = int.Parse(Console.ReadLine());
+Console.WriteLine($"result = {sum(value)}");
