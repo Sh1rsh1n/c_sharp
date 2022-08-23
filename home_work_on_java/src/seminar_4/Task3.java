113f7ebe0cc37f8a3975e9333b3d3cb28c5bb9cf
Sh1rsh1n <95686701+Sh1rsh1n@users.noreply.github.com>
2022-08-23 15:57:34 +0300

    Seminar4 add files 3 tasks

---
+++ home_work_on_java/src/seminar_4/Task3.java
@@ -0,0 +1,26 @@
+/*
+Задача 29: Напишите программу, которая задаёт массив 
+из 8 элементов и выводит их на экран.
+*/
+
+
+public class Task3{
+	
+	static void fillArray(int capacity, int max){
+		int[] array = new int[capacity];
+		
+		for(int i = 0; i < array.length; i++){
+			array[i] = (int) (Math.random() * max);
+			System.out.println("index " + i + " : " + array[i]);
+		}
+	}
+
+
+    public static void main(String args[]){
+
+        fillArray(10, 100);
+    }
+}
+
+
+
