14649bde30e7548c9ac7e8424b1a2ea190c468cb
Sh1rsh1n <95686701+Sh1rsh1n@users.noreply.github.com>
2022-08-24 15:31:37 +0300

    Add seminar 6 home work

--- Seminars/Seminar_6_HomeWork/task_1/Program.cs
+++ Seminars/Seminar_6_HomeWork/task_1/Program.cs
@@ -5,3 +5,56 @@
 
 1, -7, 567, 89, 223-> 3
 */
+
+using System;
+
+public class Program
+{
+	/*
+		fillArray(int capacity) метод создает массив чисел, capacity - начальный размер массива
+		в цикле do-while проверяем правильность ввод значения пользователем, если TryParse
+		вернет true, то цикл завершает выполнение и по данному индексу массива значение присвоенно успешно.
+	*/
+	static int[] fillArray(int capacity)
+	{
+		int[] array = new int[capacity];
+		
+		for(int i = 0; i < capacity; i++)
+		{
+			bool val;
+			do
+			{
+				Console.WriteLine("Enter more " + (capacity - i) + " numbers");
+				val = int.TryParse(Console.ReadLine(), out array[i]);
+				
+				if(val) //доп уведомление о корректности ввода значения
+				{
+					Console.WriteLine("Number is correct");
+				}
+				else
+				{
+					Console.WriteLine("Incorrect value");
+				}
+				Console.WriteLine();
+				
+			} while(!val);
+		}
+		return array;
+	}
+	
+    static public void Main ()
+    {
+		
+       	int[] arr = fillArray(9); //создаем массив и присваиваем ему результат выполнения метода fillArray()
+		int count = 0; //доп переменная счетчик
+		foreach(int i in arr) //проходим по всем элементам массива
+		{
+			if(i > 0) //если элемент массива > 0 выводим его на экран и увеличиваем переменную счетчик на 1
+			{
+				count++;
+				Console.Write(i + " ");
+			}
+		}
+		Console.WriteLine("-> amount input numbers = " + count); //выводим результат подсчетов
+    }
+}
--- Seminars/Seminar_6_HomeWork/task_2/Program.cs
+++ Seminars/Seminar_6_HomeWork/task_2/Program.cs
@@ -9,3 +9,61 @@ x = (b2 - b1) / (k1 - k2);
 y = (k1 * (b2 - b1)) / (k1 - k2) + b1;
 
 */
+
+using System;
+
+public class Program
+{
+	/*
+		метод CrossingOfLine() принимает 4 параметра типа double
+	*/
+	static void CrossingOfLine(double b1, double b2, double k1, double k2)
+	{
+		if (k1==k2 && b1==b2) //линии нахрдятся на одной прямой
+    	{
+        	Console.WriteLine("The lines is matches");
+        	return;
+   		 } 
+  		else if (k1==k2) //линии параллельны
+    	{
+        	Console.WriteLine("The lines is parallel.");
+        	return;
+    	}
+  		else //находим точку пересечения двух линий
+   		{
+      		double x = (b2 - b1) / (k1 - k2);
+			double y = (k1 * x + b1);
+			Console.Write(x + ", " + y); //выводим результат вычислений
+   		}
+	}
+	
+	/*
+		checkInput() дополнительный метод для проверки ввода значения
+		возвращает значение типа double
+		если на вход приходит неверный параметр, то выполняет цикл заново
+	*/
+	static double InputAndCheck()
+	{
+		double num;
+		bool val;
+		do
+		{
+			Console.WriteLine("Enter number: ");
+			val = double.TryParse(Console.ReadLine(), out num);
+		}
+		while(!val);
+		
+		return num;
+	}
+
+    static public void Main ()
+    {
+		
+		double b1 = InputAndCheck();
+		double b2 = InputAndCheck();
+		double k1 = InputAndCheck();
+		double k2 = InputAndCheck();
+			
+       CrossingOfLine(b1, b2, k1, k2);
+    }
+}
